c9240b3b743718052a26f2f19798da77bdee4c2f
VehsagriX <khasan.irgashev.97@gmail.com>
2025-05-27 10:49:00 +0500

    изменил зависимость api-token-header для доступа к небезопасным методам

--- url-shortener/api/api_v1/global_depensities.py
+++ url-shortener/api/api_v1/global_depensities.py
@@ -1,13 +1,13 @@
 from typing import Annotated
 
-from fastapi import Query, HTTPException, status, Request
+from fastapi import Header, HTTPException, status, Request
 
 from core.config import API_TOKENS, UNSAFE_METHODS
 
 
 def api_token_required(
     request: Request,
-    api_token: Annotated[str, Query()] = "",
+    api_token: Annotated[str, Header(alias="x-auth-token")] = "",
 ):
     if request.method not in UNSAFE_METHODS:
         return

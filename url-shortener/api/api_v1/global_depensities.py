a705becc1e513076869329c7f81a96956ab79ada
VehsagriX <khasan.irgashev.97@gmail.com>
2025-05-27 12:25:16 +0500

    Рефакторил работу api_token_required с использованием HTTPBearer

--- url-shortener/api/api_v1/global_depensities.py
+++ url-shortener/api/api_v1/global_depensities.py
@@ -1,17 +1,35 @@
+import logging
 from typing import Annotated
 
-from fastapi import Header, HTTPException, status, Request
-
+from fastapi import HTTPException, status, Request, Depends
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from core.config import API_TOKENS, UNSAFE_METHODS
+
+logger = logging.getLogger(__name__)
+
+
+security = HTTPBearer(
+    scheme_name="Static api token",
+    description="YOUR **Static API token** from the developer portal.",
+    auto_error=False,
+)
 
 
 def api_token_required(
     request: Request,
-    api_token: Annotated[str, Header(alias="x-auth-token")] = "",
+    api_token: Annotated[HTTPAuthorizationCredentials | None, Depends(security)] = None,
 ):
+    logger.info("API token: %s", api_token)
     if request.method not in UNSAFE_METHODS:
         return
-    if api_token not in API_TOKENS:
+
+    if not api_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="API token is required.",
+        )
+
+    if api_token.credentials not in API_TOKENS:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid API token",
